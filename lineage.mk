cdd3e51cd7a831eb35e7564de14a2c9b1c078938
Joey Hewitt <joey@joeyhewitt.com>
2017-10-06 20:54:17 -0700

    rebrand; remove vendor .mk ref

--- lineage.mk
+++ lineage.mk
@@ -13,14 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Replicant stuff.
+$(call inherit-product, vendor/replicant/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/replicant/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte
 
-PRODUCT_NAME := lineage_serranoltexx
+PRODUCT_NAME := replicant_serranoltexx
